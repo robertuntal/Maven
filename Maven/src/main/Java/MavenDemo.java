5d23bfe05975eb0bff7bd4d503ab88608bc5e634
Robert Untalan <robertuntal@gmail.com>
2021-06-15 11:55:58 -0700

    Maven Initial

---
+++ Maven/src/main/Java/MavenDemo.java
@@ -0,0 +1,9 @@
+
+public class MavenDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("your fucked cause this thing wont go into git"); 
+	}
+
+}
